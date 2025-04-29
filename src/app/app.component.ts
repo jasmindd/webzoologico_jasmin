c06c5da419cbdc02db2f12e0c96874bd87ba972e
cesar <cesarjavierveloza501@gmail.com>
2025-04-29 16:36:07 -0500

    Proyecto Angular get

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AnimalComponent } from './components/animal/animal.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AnimalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
